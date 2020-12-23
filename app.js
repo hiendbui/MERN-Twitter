d9eb1aae507d0a3193628f14196bf700f554a09b
hiendbui <hiendbui0@gmail.com>
2020-12-23 14:40:12 -0800

    Write test routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const express = require("express");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 const app = express();
 const db = require('./config/keys').mongoURI;
 
@@ -13,5 +16,9 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
