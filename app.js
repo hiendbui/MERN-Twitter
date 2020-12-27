882f13e1f7e811ed97ae468a13c29e54e58ab04b
hiendbui <hiendbui0@gmail.com>
2020-12-26 16:27:56 -0800

    Create user registration

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const express = require("express");
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const User = require("./models/User");
 
 mongoose
   .connect(db, { 
@@ -14,14 +14,14 @@ mongoose
      })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    debugger;
     res.send("Hello World")
 });
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
